9261d54a32ad730718069936e977895655bfaaea
Recyclingbottle <hoholeo382@gmail.com>
2022-11-28 16:50:34 +0900

    11.28

--- app/src/main/java/kr/ac/kumoh/s20180468/android_volleywithrecyclerview/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180468/android_volleywithrecyclerview/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.ac.kumoh.s20180468.android_volleywithrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumoh.s20180468.android_volleywithrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var model: SongViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+        model.requestSong()
     }
 }
